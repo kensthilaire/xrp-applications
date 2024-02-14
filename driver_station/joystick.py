9b6830c197460fa7ff5c73589af0833307381624
Ken St.Hilaire <kensthilaire@gmail.com>
2024-02-14 08:43:14 -0500

    Initial pass at a xbox controller for XRP, supports Logitech Gamepad F310

---
+++ driver_station/joystick.py
@@ -0,0 +1,96 @@
+
+from evdev import InputDevice, categorize, ecodes, KeyEvent, list_devices
+
+import logging
+from logger import logger
+
+class Joystick:
+    SUPPORTED_DEVICES = (
+        "Logitech Gamepad F310"
+        )
+
+    BUTTONS = {
+        304: { 'name': 'ButtonA' },
+        305: { 'name': 'ButtonB' },
+        307: { 'name': 'ButtonX' },
+        308: { 'name': 'ButtonY' },
+        310: { 'name': 'LeftBumper' },
+        311: { 'name': 'RightBumper' },
+        314: { 'name': 'Select' },
+        315: { 'name': 'Start' },
+        317: { 'name': 'LeftThumb' },
+        318: { 'name': 'RightThumb' }
+    }
+
+    BUTTON_STATES = {
+        0: 'RELEASED',
+        1: 'PRESSED'
+    }
+
+    AXIS_TYPES = {
+        0:  { 'name': 'LeftJoystickX', 'min': -32768, 'max': 32767 },
+        1:  { 'name': 'LeftJoystickY', 'min': -32768, 'max': 32767 },
+        2:  { 'name': 'LeftTrigger', 'min': 0, 'max': 255 },
+        3:  { 'name': 'RightJoystickX', 'min': -32768, 'max': 32767 },
+        4:  { 'name': 'RightJoystickY', 'min': -32768, 'max': 32767 },
+        5:  { 'name': 'RightTrigger', 'min': 0, 'max': 255 },
+        16: { 'name': 'HatX', 'min': -1, 'max': 1 },
+        17: { 'name': 'HatY', 'min': -1, 'max': 1 }
+    }
+
+    def __init__(self, path=None):
+        self.gamepad = None
+
+        if path:
+            self.gamepad = InputDevice(path)
+        else:
+            devices = [InputDevice(path) for path in list_devices()]
+            for device in devices:
+                if device.name in self.SUPPORTED_DEVICES:
+                    logger.info( device )
+                    self.gamepad = InputDevice(device.path)
+                    break
+
+    def decode_event(self, event):
+        decoded_event = { 'type': 'UNKNOWN', 'name': '', 'value': event.value }
+
+        if event.type == ecodes.EV_SYN:
+            decoded_event['type'] = 'SYN'
+        elif event.type == ecodes.EV_KEY:
+            decoded_event['type'] = 'BUTTON'
+            button = self.BUTTONS.get(event.code, None)
+            if button:
+                decoded_event['name'] = button['name']
+                decoded_event['value'] = event.value
+        elif event.type == ecodes.EV_ABS:
+            decoded_event['type'] = 'AXIS'
+            axis = self.AXIS_TYPES.get(event.code, None)
+            if axis:
+                decoded_event['name'] = axis['name']
+                decoded_event['value'] = event.value / axis['max']
+                decoded_event['rounded_value'] = round((event.value / axis['max']), 2)
+
+        return decoded_event
+
+    def read(self):
+        for event in self.gamepad.read_loop():
+            decoded_event = self.decode_event( event )
+            if decoded_event['type'] == 'SYN':
+                # ignore the SYN event types, not much to do with them
+                pass
+            elif decoded_event['type'] == 'BUTTON':
+                processed_state = self.BUTTON_STATES.get(decoded_event['value'], 'UNKNOWN')
+                logger.debug( 'Button Type: %s, Value: %s' % (decoded_event['name'], processed_state) )
+            elif decoded_event['type'] == 'AXIS':
+                logger.debug( 'Axis Type: %s, Value: %f' % (decoded_event['name'], decoded_event['value']) )
+            else:
+                logger.info( 'Unknown Event Type: %d, Code: %d, Value: %f' % (event.type, event.code, event.value) )
+
+if __name__ == '__main__':
+    logger.setLevel(logging.DEBUG)
+
+    joystick = Joystick()
+    try:
+        joystick.read()
+    except KeyboardInterrupt:
+        logger.info( 'Terminating Joystick Session' )
---
+++ driver_station/xrp_controller.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import socket
+import signal
+import sys
+import time
+
+from config import read_config
+
+from logger import logger
+from joystick import Joystick
+
+# dictionary of all the xbox controller buttons and controls. By enabling or disabling
+# the controls, you can control how much extra traffic is sent down to the XRP.
+controls = {
+    'ButtonA':        { 'type': 'BUTTON', 'enabled': True },
+    'ButtonB':        { 'type': 'BUTTON', 'enabled': True  },
+    'ButtonX':        { 'type': 'BUTTON', 'enabled': True  },
+    'ButtonY':        { 'type': 'BUTTON', 'enabled': True  },
+    'LeftBumper':     { 'type': 'BUTTON', 'enabled': True  },
+    'RightBumper':    { 'type': 'BUTTON', 'enabled': True  },
+    'Select':         { 'type': 'BUTTON', 'enabled': True  },
+    'Start':          { 'type': 'BUTTON', 'enabled': True  },
+    'LeftThumb':      { 'type': 'BUTTON', 'enabled': True  },
+    'RightThumb':     { 'type': 'BUTTON', 'enabled': True  },
+    'LeftJoystickX':  { 'type': 'AXIS',   'enabled': True  },
+    'LeftJoystickY':  { 'type': 'AXIS',   'enabled': True  },
+    'LeftTrigger':    { 'type': 'AXIS',   'enabled': True  },
+    'RightJoystickX': { 'type': 'AXIS',   'enabled': True  },
+    'RightJoystickY': { 'type': 'AXIS',   'enabled': True  },
+    'RightTrigger':   { 'type': 'AXIS',   'enabled': True  },
+    'HatX':           { 'type': 'AXIS',   'enabled': True  },
+    'HatY':           { 'type': 'AXIS',   'enabled': True  }
+}
+
+#
+# class to implement the XRP controller. This class is derived from the Joystick class
+# and supports an Xbox Controller connected via USB to a Raspberry Pi
+#
+class XrpController(Joystick):
+    def __init__(self, path=None, socket_type='UDP', host='', team_number=9999):
+        super().__init__(path)
+
+        signal.signal(signal.SIGINT, self.shutdown)
+        signal.signal(signal.SIGTERM, self.shutdown)
+
+        self.host = host
+        self.port = team_number
+        self.socket = None
+        self.socket_type = socket_type
+
+        self.initialize_client_socket()
+
+    def initialize_client_socket(self):
+        # create a socket based on the requested type
+        logger.info( 'Creating Client Connection to %s:%d' % (self.host,self.port) )
+        if self.socket_type == 'UDP':
+            self.socket = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+        elif self.socket_type == 'TCP':
+            while True:
+                try:
+                    self.socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+                    self.socket.connect( (self.host,self.port) )
+                    logger.info( 'Client Connection Established to %s:%d' % (self.host,self.port) )
+                    break
+                except ConnectionRefusedError:
+                    time.sleep(5)
+                    logger.error( 'Error Connecting to %s:%d, Retrying' % (self.host,self.port) )
+
+    def shutdown( self, *args ):
+        logger.info( 'Shutdown complete.' )
+
+        sys.exit(0)
+
+    def send_event( self, event ):
+        command = None
+        name = event['name']
+        try:
+            control = controls[name]
+
+            if control.get('enabled', False) == True:
+                if control['type'] == 'AXIS':
+                    # for the axis type, send the value rounded to the nearest 2 decimal points
+                    value = event['rounded_value']
+                    command = '%s:%s:%f' % ('Event',name, value) 
+                elif control['type'] == 'BUTTON':
+                    # for the button type, send the value reported by the button (1:PRESSED or 0:RELEASED)
+                    value = event['value']
+                    command = '%s:%s:%d' % ('Event',name, value) 
+                else:
+                    logger.error( 'Unknown Event Type: %s' % name )
+
+                if command:
+                    logger.debug( 'Sending: %s' % command )
+                    command += '\n'
+                    if self.socket_type == 'UDP':
+                        self.socket.sendto( command.encode('utf-8'), (self.host,self.port) )
+                    if self.socket_type == 'TCP':
+                        self.socket.sendall( command.encode('utf-8') )
+
+        except KeyError:
+            pass
+
+    def joystick_control(self):
+        for event in self.gamepad.read_loop():
+            try:
+                decoded_event = self.decode_event( event )
+                self.send_event( decoded_event )
+            except BrokenPipeError:
+                logger.error( 'Client Connection Lost to %s:%d, Retrying' % (self.host,self.port) )
+                self.initialize_client_socket()
+
+
+if __name__ == '__main__':
+
+    #
+    # parse out the command arguments
+    #
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true', dest='debug', default=False)
+    parser.add_argument('-c', '--config', action='store', dest='config', default='config.json')
+    parser.add_argument('-s', '--socket', action='store', dest='socket_type', default=None)
+    parser.add_argument('-t', '--team', action='store', dest='team', default=None)
+    parser.add_argument('-x', '--xrp', action='store', dest='xrp_ipaddr', default=None)
+    options = parser.parse_args()
+
+    #
+    # Read the config file
+    config = read_config( filename=options.config )
+
+    # set the log level to debug if requested
+    if options.debug or config.get('debug',False) == True:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    # retrieve the XRP IP server address and port number (team) from config or
+    # from the command line
+    if options.xrp_ipaddr:
+        xrp_ipaddr = options.xrp_ipaddr
+    else:
+        xrp_ipaddr = config.get('xrp_ipaddr', 'localhost')
+
+    if options.team:
+        team = options.team
+    else:
+        team = config.get('team', 9999)
+
+    if options.socket_type:
+        socket_type = options.socket_type.upper()
+    else:
+        socket_type = config.get('socket_type', 'UDP').upper()
+
+    #
+    # Create the XRP controller instance
+    controller = XrpController(socket_type=socket_type, host=xrp_ipaddr, team_number=team)
+
+    try:
+        # invoke the controller type as configured. Initially, an Xbox Controller is supported,
+        # but other controller methods will be added over time
+        if config['controller'] == 'joystick':
+            controller.joystick_control()
+        else:
+            logger.error( 'ERROR: No Controller Type Specified' )
+            sys.exit(1)
+
+    except KeyboardInterrupt:
+        controller.shutdown();
+
